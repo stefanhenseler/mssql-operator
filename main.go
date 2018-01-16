892d8e73c89da66d58313ee5d88ddf30746c0bd3
Stefan Henseler <stefan.henseler@synax.ch>
2018-01-16 21:05:35 +0100

    Update do code for test

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"io"
 	"net/http"
 )
 
-type Calculator struct {
-	Op1, Op2 int
-	IsAdd    bool
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
 }
 
 func main() {
-	http.HandleFunc("/calculate", calchandler)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func calchandler(w http.ResponseWriter, r *http.Request) {
-	var req Calculator
-	dec := json.NewDecoder(r.Body)
-	err := dec.Decode(&req)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-
-	isadd := req.IsAdd
-	op1 := req.Op1
-	op2 := req.Op2
-
-	if isadd {
-		fmt.Fprintf(w, "%d", op1-op2)
-	} else {
-		fmt.Fprintf(w, "%d", op1+op2)
-	}
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
 }
